34fc079a1186d66a67a9ddb033995288b48b635c
Harshabh Gupta <85529758+Harshabh1298@users.noreply.github.com>
2021-10-04 15:54:28 +0530

    Update sqlfileportfolio.sql

--- sqlfileportfolio.sql
+++ sqlfileportfolio.sql
@@ -1,5 +1,6 @@
 -- a total analysis final using joins
-select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths, round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
+select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths,
+round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
 B.total_vaccination_per_hundred, round((A.total_cases/A.POPULATION)*100,2) percent_of_cases_per_population,
 round((a.total_deaths/A.total_cases)*100,2) as percent_death_per_cases
 from (select location, population, Max(total_cases) as total_cases, Max(total_deaths) as total_deaths
@@ -15,9 +16,10 @@ and total_cases is not null
 order by a.population desc;
 
 -- a total analysis final for continent 
-select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths, round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
- B.total_vaccination_per_hundred,
-round((A.total_cases/A.POPULATION)*100,2) percent_of_cases_per_population, round((a.total_deaths/A.total_cases)*100,2) as percent_death_per_cases
+select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths, 
+round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
+B.total_vaccination_per_hundred, round((A.total_cases/A.POPULATION)*100,2) percent_of_cases_per_population, 
+round((a.total_deaths/A.total_cases)*100,2) as percent_death_per_cases
 from (select location, population, Max(total_cases) as total_cases, Max(total_deaths) as total_deaths
 from covid_deaths
 where continent is null
@@ -32,9 +34,10 @@ and total_cases is not null
 order by a.population desc;
 
 -- final analysis for countries
-select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths, round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
- B.total_vaccination_per_hundred,
-round((A.total_cases/A.POPULATION)*100,2) percent_of_cases_per_population, round((a.total_deaths/A.total_cases)*100,2) as percent_death_per_cases
+select  A.LOCATION, A.POPULATION, A.total_cases, a.total_deaths, 
+round(((B.total_vaccination_per_hundred/100)*a.population),2) as total_vaccine_given,
+B.total_vaccination_per_hundred, round((A.total_cases/A.POPULATION)*100,2) percent_of_cases_per_population, 
+round((a.total_deaths/A.total_cases)*100,2) as percent_death_per_cases
 from (select location, population, Max(total_cases) as total_cases, Max(total_deaths) as total_deaths
 from covid_deaths
 where continent is not null
